b94ab50effa4daeff813a773371c163c2ffc361b
chinmayembedded <chinmayssawant@live.com>
2016-04-08 02:55:46 +0800

    Update Humour.php

--- Humour.php
+++ Humour.php
@@ -11,9 +11,39 @@
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<style>
+		h1{
+			    padding-top: 4%;
+				padding-left: 7%;
+				font-size: -webkit-xxx-large;
+				
+				color: whitesmoke;
+		}
+		p{
+			padding-top: 4%;
+			padding-left: 7%;
+			color: white;
+			font-size: x-large;
+		}
+	</style>
 </head>
 
-<body background="images/under.jpg">
-
+<body style="background-color:black">
+	<h1>
+		HUMOUR
+	</h1>
+	<p>
+		A scientist can discover a new star, but he cannot make one. He would have to ask an engineer to do that.
+	</p>
+	
+	<p>
+		The world does revolve around us... We pick the coordinate system.
+	</p>
+	<p>
+		We threw the whole thing out and are starting from scratch.
+	</p>
+	<p>
+		We haven't started this job yet, but we've got to say something.
+	</p>
 </body>
 </html>
